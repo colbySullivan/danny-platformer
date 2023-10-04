a8cd8f14775894fa14e515cad16ef2d755900f46
Colby Sullivan <sullivanc@southwestern.edu>
2023-10-04 09:00:49 -0500

    Sprite Animation

--- Logic/KBExample.cs
+++ Logic/KBExample.cs
@@ -6,26 +6,43 @@ public class KBExample : KinematicBody2D
 	[Signal]
 	public delegate void playerHit();
 	
+	private AnimatedSprite _animatedSprite;
+	
 	public int Speed = 250;
 	private Vector2 _velocity = new Vector2();
 
 	public void GetInput()
 	{
+		_animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+		
 		// Detect up/down/left/right keystate and only move when pressed
 		_velocity = new Vector2();
 
 		if (Input.IsActionPressed("ui_right"))
+		{
 			_velocity.x += Speed;
-			//set_frame(1); // Doesn't work
+			_animatedSprite.Play("right");
+		}
 
 		if (Input.IsActionPressed("ui_left"))
+		{
 			_velocity.x -= Speed;
+			_animatedSprite.Play("left");
+		}
+			
 
 		if (Input.IsActionPressed("ui_down"))
+		{
 			_velocity.y += Speed;
+			_animatedSprite.Play("down");
+		}
+			
 
 		if (Input.IsActionPressed("ui_up"))
+		{
 			_velocity.y -= Speed;
+			_animatedSprite.Play("up");
+		}
 	}
 
 	public override void _PhysicsProcess(float delta)
